33a0f39c69723211e01baec55fee8e69d06c2b58
Guru <team@tsglobaltech.com>
2025-05-26 05:48:05 +0530

    signin-form

---
+++ public/thm/model-signin.js
@@ -0,0 +1,33 @@
+function signInModel(){
+// Get the modal
+var modal = document.getElementById("signInModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("signIn");
+var btnLg = document.getElementById("signInLg");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-sign-in-model")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  closeNav();
+  modal.style.display = "block";
+}
+btnLg.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
+signInModel()
